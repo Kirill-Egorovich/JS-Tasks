3647787d34e57cf6c2cba3e8310f22830924f355
Kirill-Egorovich <kirillegorovich07@gmail.com>
2024-06-14 14:35:25 +0500

    Update 3.js

--- 3.js
+++ 3.js
@@ -58,3 +58,32 @@ let numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70];
 for (let key in numbers){
     console.log('Индексу ' + key + ' соответствует число ' + numbers[key]);
 }
+
+
+
+/* Задание 5
+Дан массив объектов, например:
+let questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+Добавить в каждый объект дополнительное поле usersAnswer со значением null.
+Решение должно работать для массива любой длины. 
+*/
+
+let questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0,
+    usersAnswer: null
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0,
+    usersAnswer: null
+}];
