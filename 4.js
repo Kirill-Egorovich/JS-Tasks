0d0de1af7a921f7156ac2a0955bdbf10f1e99dba
Kirill-Egorovich <kirillegorovich07@gmail.com>
2024-06-23 12:54:15 +0500

    Update 4.js

--- 4.js
+++ 4.js
@@ -53,3 +53,57 @@ for (let i = 0; i < roles.length; i++) {
 }
 
 console.log(newRoles);
+
+
+
+/* Задание 4
+Создайте функцию findPositiveNumbers(). Она принимает массив в качестве параметра и возвращает новый массив, в котором содержатся только положительны элементы.
+Проверьте работу функции на примере массивов:
+    const nums = [5, 4, 3, -3, -10, -1, 8, -20, 0]
+    const numbers = [443, -39, 0, -105, -21, 38, -20, 3, 90].
+*/
+
+const nums = [5, 4, 3, -3, -10, -1, 8, -20, 0];
+
+function findPositiveNumbers(nums){
+    let positiveNumbers = [];
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] > 0) {
+            positiveNumbers.push(nums[i]);
+        }
+    }
+
+    return positiveNumbers;
+}    
+console.log(findPositiveNumbers(nums));
+
+
+const numbers = [443, -39, 0, -105, -21, 38, -20, 3, 90];
+
+function findPositiveNumbersTwo(numbers){
+    let positiveNumbersTwo = [];
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] > 0) {
+            positiveNumbersTwo.push(numbers[i]);
+        }
+    }
+
+    return positiveNumbersTwo;
+} 
+console.log(findPositiveNumbersTwo(numbers));
+
+
+
+/* Задание 5
+Напишите функцию, которая принимает два параметра: значение и количество элементов. Функция создает новый массив и заполняет его этим значением в указанном количестве.
+Например: из переданных значений ('a', 3) получаем массив ['a', 'a', 'a'] 
+*/
+
+function valueInQuantity(meaning, quantity){
+    const result = [];
+    for (let i = 0; i < quantity; i++) {
+        result.push(meaning);
+    }
+    return result;
+}
+console.log(valueInQuantity('Kirill', 3));
