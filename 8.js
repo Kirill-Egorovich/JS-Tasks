85123dcd4ba17d5725c4ec79b4fbccf49039fdb3
Kirill-Egorovich <kirillegorovich07@gmail.com>
2024-07-17 16:04:40 +0500

    Update 8.js

--- 8.js
+++ 8.js
@@ -31,3 +31,35 @@ listItems.forEach(item => console.log(item.innerText));
 */
 
 listItems.forEach((item, index) => item.innerText = index);
+
+
+
+/* Задание 4
+Создать с помощью js абзац (тег p). Добавить в него контент. Добавить к нему стили: размер 36px, жирный шрифт. Добавить абзац с текстом на страницу. 
+*/
+
+const paragraph = document.createElement('p');
+paragraph.style.fontSize = '36px';
+paragraph.style.fontWeight = 'bold';
+
+document.body.append(paragraph);
+
+
+
+/* Задание 5
+Написать в html тег заголовка с текстом "Сегодня:" 
+После заголовка подготовить в html пустой параграф. С помощью js добавить в него сегодняшнюю дату. 
+*/
+
+// Не работает -->
+const forDate = document.querySelector('.for-Date');
+
+let now = new Date();
+
+let options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
+
+forDate.innerText = now.toLocaleDateString('ru-RU', options);
